6b8abdff8a22bf4c606aa8ea7467c60a7fe9023c
Jiahao <lucashuamani256w@gmail.com>
2024-03-11 00:12:09 -0400

    Implement models

---
+++ src/models/comments.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  content: { type: String, required: true },
+  date: { type: Date, required: true, default: Date.now() },
+  user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+});
+
+PostSchema.virtual("date_formatted").get(function () {
+  return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATE_FULL);
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
---
+++ src/models/post.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  date: { type: Date, required: true, default: Date.now() },
+  author: { type: Schema.Types.ObjectId, required: true, ref: "Author" },
+});
+
+PostSchema.virtual("date_formatted").get(function () {
+  return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATE_FULL);
+});
+
+module.exports = mongoose.model("Post", PostSchema);
